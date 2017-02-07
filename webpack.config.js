80dca0bc1bafebee6d25183ccf3a3386f59ae448
Tree.Xie <vicansocanbico@gmail.com>
2017-02-07 22:15:54 +0800

    update web pack  config for support `NODE_ENV`

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,11 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
       // filename: "vendor.js"
