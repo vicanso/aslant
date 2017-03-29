6ee1608be6e42e0a635236a8b6c40fa19e282cde
Tree.Xie <vicansocanbico@gmail.com>
2017-03-29 21:11:06 +0800

    add `fork me` link

--- public/js/containers/home.js
+++ public/js/containers/home.js
@@ -113,6 +113,24 @@ class Home extends Component {
         { this.renderDashboards() }
         <h3>Configs</h3>
         { this.renderConfigs() }
+        <a
+          href="https://github.com/vicanso/aslant"
+          style={{
+            position: 'fixed',
+            right: 0,
+            bottom: 0,
+            transform: 'rotate(180deg)',
+          }}
+        >
+          <img
+            src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67"
+            alt="Fork me on GitHub"
+            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"
+            style={{
+              display: 'block',
+            }}
+          />
+        </a>
       </div>
     );
   }
